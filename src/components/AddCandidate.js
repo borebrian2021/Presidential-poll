d2f26b2a64f647add7207393133ecb2f8d3bba05
borebrian2021 <bkimutai2021@gmail.com>
2022-09-08 21:36:07 +0300

    Posting of data done

--- src/components/AddCandidate.js
+++ src/components/AddCandidate.js
@@ -1,13 +1,55 @@
 import React ,{useState} from "react";
+const BASE_URL="http://localhost:9292"
 //STATES
-const [candidateName, setCandidateName] = useState("");
+
+
+
+
+
+
+function AddCandidate() {
+let [candidateName, setCandidateName] = useState("");
 const [partyName, setPartyName] = useState("");
 const [profileImage, setProfileImage] = useState("");
 const [partyLogo,setPartyLogo]=useState("")
 
-
 //STOPT FORM FROM REFRESH
-function AddCandidate() {
+const handleSubmit=(event)=>{
+    event.preventDefault()
+    alert(candidateName)
+    fetch(BASE_URL+"/addParty",{
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+        },
+        body:JSON.stringify(
+            {
+                partyName:partyName,
+                profileImage:profileImage,
+                partyLogo:partyLogo,
+                profileImage:profileImage
+            }
+        )
+    }).then((feedback)=>feedback.json())
+    .then((data)=>{
+        alert(data.partyName)
+    })
+    
+    }
+
+//HANDLING INPUT CHANGES
+const handleCandidate=(event)=>{
+setCandidateName(event.target.value)
+}
+const handlePartyName=(event)=>{
+    setPartyName(event.target.value)
+    }
+const handleProfileImage=(event)=>{
+        setProfileImage(event.target.value)
+}
+const handlePartyLogo=(event)=>{
+    setPartyLogo(event.target.value)
+}
 
     return (
         <div className="home">
@@ -17,19 +59,19 @@ function AddCandidate() {
 
             </div>
             <div className="pollsResults">
-                <form class="register" >
+                <form class="register" onSubmit={handleSubmit}>
                     <div className="row">
                         <div className="col-lg-4 col-md-3 col-6 ">
-                            <input class="form-control m-1" required placeholder="Enter candidate name"></input>
+                            <input value={candidateName} onChange={handleCandidate} class="form-control m-1" required placeholder="Enter candidate name"></input>
                         </div>
                         <div className="col-lg-4 col-md-3 col-6 ">
-                            <input class="form-control m-1" required placeholder="Enter party name"></input>
+                            <input class="form-control m-1" value={partyName} onChange={handlePartyName} required placeholder="Enter party name"></input>
                         </div> 
                         <div className="col-lg-4 col-md-3 col-6 ">
-                            <input class="form-control m-1" required placeholder="Enter image url"></input>
+                            <input class="form-control m-1" value={profileImage} required onChange={handleProfileImage} placeholder="Enter image url"></input>
                         </div>
                         <div className="col-lg-4 col-md-3 col-6 ">
-                            <input class="form-control m-1" required placeholder="Enter party logo url"></input>
+                            <input class="form-control m-1" required value={partyLogo} onChange={handlePartyLogo} placeholder="Enter party logo url"></input>
                         </div>
                         <div className="col-lg-4 col-md-3 col-6  ">
                             <button class="optionButtons mt-2" type="submit">Submit</button>
