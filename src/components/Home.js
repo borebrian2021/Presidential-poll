4422e0cd572795328d5163154757514bbc3f23ba
borebrian2021 <bkimutai2021@gmail.com>
2022-09-08 13:18:54 +0300

    Added poll image

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,12 @@ function Home(){
     return(
 <div className="home">
 <div className="appTitle">
-<h6>Kenya Presidential Poll</h6>
+<h6>Kenya Presidential Poll Results</h6>
     </div>
     <div className="pollsResults">
-
+<div className="imageSecetion">
+<img src="https://iili.io/6Lue1a.jpg" className="poll_image" alt="6Lue1a.jpg" border="0"/>
+</div>
     </div>
 </div>
 
