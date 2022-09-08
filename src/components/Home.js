1e5566953984555915d894fd97473b6af3661bb0
borebrian2021 <bkimutai2021@gmail.com>
2022-09-08 13:07:27 +0300

    Added app title

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,12 @@ function Home(){
 
     return(
 <div className="home">
+<div className="appTitle">
+<h6>Kenya Presidential Poll</h6>
+    </div>
+    <div className="pollsResults">
 
+    </div>
 </div>
 
     )
